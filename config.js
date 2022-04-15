03f700e81fed3b9549f245b8c168caa20ed4a01d
Sharon Blessy <sharonblessy5@gmail.com>
2022-04-15 09:58:15 +0530

    config changes

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyACNznRBSPxAKhhVVmljWfJNxW4ox5OtRA",
+    authDomain: "p67v3-42758.firebaseapp.com",
+    databaseURL: "https://p67v3-42758-default-rtdb.firebaseio.com",
+    projectId: "p67v3-42758",
+    storageBucket: "p67v3-42758.appspot.com",
+    messagingSenderId: "642908604799",
+    appId: "1:642908604799:web:e763e671f05dcab7e0724a"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
